55b8f9424446cf6d1f00872286b9e1ef776c454e
Jakub Porębski <29267@s.pm.szczecin.pl>
2024-05-15 11:02:08 +0200

    Piece skrypt

--- Assets/Skrypty/Tablica.cs
+++ Assets/Skrypty/Tablica.cs
@@ -1,9 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Tablica : MonoBehaviour
 {
+    public Tilemap tilemap;
     public Tetrodata[] tetrisy;
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        for (int i = 0; i < tetrisy.Length; i++)
+        {
+            this.tetrisy[i].Initialize();
+        }
+    }
+
+    public void Start()
+    {
+        Spawnelement();
+    }
+    public void Spawnelement()
+    {
+        int random = Random.Range(0, this.tetrisy.Length);
+        Tetrodata data = this.tetrisy[random];
+
+    }
+
+    public void Set()
+    {
+
+    }
 
 }
