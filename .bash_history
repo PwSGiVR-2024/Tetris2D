083470d2a48a7ec3c61e27bb1d53d7164eee1075
Jakub Porębski <29267@s.pm.szczecin.pl>
2024-05-28 21:32:51 +0200

    system spawnu dla elementów

---
+++ .bash_history
@@ -0,0 +1,13 @@
+git add .
+git commit -m "nowe tilles"
+git config --global user.email "29267@s.pm.szczecin.pl"
+git commit -m "nowe tilles"
+git push
+git fetch
+git pull
+git push
+git sstatus
+git status
+git add .
+git commit -m "Piece skrypt"
+git push
--- Assets/Skrypty/Tablica.cs
+++ Assets/Skrypty/Tablica.cs
@@ -7,9 +7,12 @@ public class Tablica : MonoBehaviour
 {
     public Tilemap tilemap;
     public Tetrodata[] tetrisy;
+    public Piece activePiece;
+    public Vector3Int spawnPosition;
     private void Awake()
     {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
         for (int i = 0; i < tetrisy.Length; i++)
         {
             this.tetrisy[i].Initialize();
@@ -24,12 +27,18 @@ public class Tablica : MonoBehaviour
     {
         int random = Random.Range(0, this.tetrisy.Length);
         Tetrodata data = this.tetrisy[random];
-
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
     }
 
-    public void Set()
+    public void Set(Piece piece)
     {
-
+        for (int i = 0; i < piece.kratka.Length; i++)
+        {
+            Vector3Int tilePosition = piece.kratka[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
     }
+
 
 }
